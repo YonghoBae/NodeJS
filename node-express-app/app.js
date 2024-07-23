6bbf8545149e9c42115038ce693fba67ed9d8d17
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-07-23 11:16:06 +0900

    24-07-23

--- node-express-app/app.js
+++ node-express-app/app.js
@@ -1,3 +1,6 @@
+//npm start --> bin/www.js실행 --> app.js --> routing 모듈 호출 --> 라우팅메소드 호출
+//-->DATA(Model) 또는 
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
